19361aebb9c8ec5b15f2047fd13c7d29248eb1c8
Mandeep Sandhu <mandeep2sandhu@gmail.com>
2019-03-24 14:00:54 -0700

    Add files via upload

---
+++ GeorgianYearCal.py
@@ -0,0 +1,13 @@
+﻿year = int(input("Enter a year: "))
+#
+# put your code here
+#
+if year<1582:
+    print("Not within Gregorian calendar's period")
+elif year%4 != 0:
+    print("Common Year")
+elif year%100 != 0:
+    print("Leap Year")
+elif year%400 !=0:
+    print("Common Year")
+else: print("Leap Year")
